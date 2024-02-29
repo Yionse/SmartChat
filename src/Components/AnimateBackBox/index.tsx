2844d1835bacb5de10614cb69f2e1235a49f410f
Zhangtiancheng <2458015575@qq.com>
2024-02-29 17:29:10 +0800

    :sparkles: 登录页布局完毕

---
+++ src/Components/AnimateBackBox/index.tsx
@@ -0,0 +1,46 @@
+import React, {useEffect, useRef, useState} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
+export default function AnimateBackBox(props: any) {
+  const [locations, setLocations] = useState<number[]>([0.1, 0.9]);
+  const [isAdd, setIsAdd] = useState<boolean>(true);
+  const timer = useRef<null>();
+  (() => {
+    clearInterval(timer.current as any);
+    timer.current = setInterval(() => {
+      let [x, y] = locations;
+      if (isAdd) {
+        x += 0.01;
+        y -= 0.01;
+        if (x >= 0.41) {
+          setIsAdd(false);
+        }
+      } else {
+        x -= 0.01;
+        y += 0.01;
+        if (x <= 0.1) {
+          setIsAdd(true);
+        }
+      }
+      setLocations(() => [x, y]);
+    }, 150) as any;
+  })();
+  useEffect(() => {
+    return () => clearInterval(timer.current as any);
+  }, []);
+  return (
+    <LinearGradient
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'flex-end',
+      }}
+      colors={['#cffafe', '#22d3ee']}
+      locations={locations}
+      start={{x: 0.99, y: 0}}
+      end={{x: 0, y: 0.99}}>
+      {props.children}
+    </LinearGradient>
+  );
+}
--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,18 +1,116 @@
-import React from 'react';
-import {useNavigation} from '@react-navigation/native';
-import {Button, Text} from 'native-base';
+import React, {useEffect, useRef, useState} from 'react';
+import {Box, Button, Input, Text, Toast, View} from 'native-base';
+import {StyleSheet} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import AnimateBackBox from '../Components/AnimateBackBox';
 
 export default function Login() {
-  const navigation = useNavigation();
+  const [user, setUser] = useState<string>('');
+  const [code, setCode] = useState<string>('');
+  const [isDisabledCodeBtn, setIDisabledCodeBtn] = useState(false);
+  const [count, setCount] = useState(59);
+  const btnTimer = useRef<null>();
+  const styles = StyleSheet.create({
+    titleText: {
+      height: 60,
+      lineHeight: 60,
+      fontSize: 30,
+    },
+  });
+
+  useEffect(() => {
+    return () => clearInterval(btnTimer.current as any);
+  }, []);
+
+  const codeBtnHandle = () => {
+    setIDisabledCodeBtn(true);
+    clearInterval(btnTimer.current as any);
+    btnTimer.current = setInterval(() => {
+      setCount(countDown => countDown - 1);
+    }, 1000) as any;
+  };
+
+  useEffect(() => {
+    if (count < 1) {
+      setCount(60);
+      setIDisabledCodeBtn(false);
+      clearInterval(btnTimer.current as any);
+    }
+  }, [count]);
+
+  const loginHandle = () => {
+    const regex = /^\d+$/;
+    if (regex.test(user) && regex.test(code) && code.length === 6) {
+      console.log(user, code);
+    } else {
+      Toast.show({
+        description: '请输入合法的QQ或验证码',
+      });
+    }
+  };
+
   return (
-    <>
-      <Text>登录</Text>
-      <Button
-        onPress={() => {
-          navigation.navigate('Home' as never);
+    <AnimateBackBox>
+      <Text style={styles.titleText} marginLeft={18} color="white">
+        Welcome to{' '}
+      </Text>
+      <Text style={styles.titleText} marginLeft={30} color="white">
+        智聊 App{' '}
+      </Text>
+      <Box
+        style={{
+          width: '100%',
+          height: '45%',
+          borderTopLeftRadius: 40,
+          borderTopRightRadius: 40,
+          backgroundColor: '#fff',
+          display: 'flex',
+          justifyContent: 'space-around',
         }}>
-        去Home
-      </Button>
-    </>
+        <Text style={styles.titleText} marginLeft={30} color={'black'}>
+          Login
+        </Text>
+        <View
+          style={{
+            padding: 10,
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}>
+          <Input
+            maxLength={13}
+            borderRadius={22}
+            InputLeftElement={
+              <AntDesign name="user" size={18} style={{marginLeft: 8}} />
+            }
+            placeholder="输入QQ将自动使用邮箱登录"
+            value={user}
+            onChangeText={setUser}
+          />
+          <Input
+            value={code}
+            onChangeText={setCode}
+            type="Number"
+            maxLength={6}
+            borderRadius={22}
+            my={4}
+            InputLeftElement={
+              <AntDesign name="eye" size={18} style={{marginLeft: 8}} />
+            }
+            InputRightElement={
+              <Button
+                style={{width: 100, marginRight: 16}}
+                isDisabled={isDisabledCodeBtn}
+                onPress={codeBtnHandle}>
+                {isDisabledCodeBtn ? `${count}秒后重试` : '发送验证码'}
+              </Button>
+            }
+            placeholder="输入邮箱验证码"
+          />
+          <Button borderRadius={22} onPress={loginHandle}>
+            登录
+          </Button>
+        </View>
+      </Box>
+    </AnimateBackBox>
   );
 }
